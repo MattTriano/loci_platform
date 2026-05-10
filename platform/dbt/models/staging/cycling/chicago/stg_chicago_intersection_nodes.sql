95671e5c0dc02528e77df714c5512981cdcc62ee
matttriano <matt.triano@gmail.com>
2026-05-10 08:51:56 -0500

    Updating Chicago staging models

--- platform/dbt/models/staging/cycling/chicago/stg_chicago_intersection_nodes.sql
+++ platform/dbt/models/staging/cycling/chicago/stg_chicago_intersection_nodes.sql
@@ -1,21 +1,4 @@
 -- loci_platform/platform/dbt/models/staging/cycling/chicago/stg_chicago_intersection_nodes.sql
 {{ config(materialized='table') }}
 
-with node_way_counts as (
-    select
-        node_id,
-        count(distinct way_id) as way_count
-    from {{ ref('stg_chicago_way_nodes') }}
-    group by node_id
-),
-endpoints as (
-    select node_id
-    from {{ ref('stg_chicago_way_nodes') }}
-    where position = 1 or position = way_length
-)
-select distinct node_id
-from (
-    select node_id from node_way_counts where way_count >= 2
-    union
-    select node_id from endpoints
-) t
+{{ generate_stg_city_intersection_nodes_model('chicago') }}
--- platform/dbt/models/staging/cycling/chicago/stg_chicago_way_nodes.sql
+++ platform/dbt/models/staging/cycling/chicago/stg_chicago_way_nodes.sql
@@ -1,10 +1,4 @@
 -- loci_platform/platform/dbt/models/staging/cycling/chicago/stg_chicago_way_nodes.sql
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
-select
-    osm_id as way_id,
-    ordinality as position,
-    node_id,
-    array_length(node_ids, 1) as way_length
-from {{ ref('stg_chicago_bike_ways') }},
-     unnest(node_ids) with ordinality as t(node_id, ordinality)
+{{ generate_stg_city_way_nodes_model('chicago') }}
