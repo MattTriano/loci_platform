29839bc31d3f63eeda0f35ebe098c706a2ccdbbf
matttriano <matt.triano@gmail.com>
2026-05-19 16:19:49 -0500

    Scaling up memory allocation to get larger fractional vCPU allocations (as the algo is CPU bound)

--- infra/variables.tf
+++ infra/variables.tf
@@ -46,8 +46,8 @@ variable "routing_lambda_memory_by_city" {
   description = "Per-city memory (MB) for the routing Lambda. Must include every city in var.cities."
   type        = map(number)
   default = {
-    chicago = 400
-    detroit = 400
-    sf      = 600
+    chicago = 1024
+    detroit = 1024
+    sf      = 1536
   }
 }
